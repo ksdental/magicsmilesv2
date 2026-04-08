22f30b0999cb51d8152d6b1e06b8d733553dd520
ksdental <developers@kickstartdental.com>
2026-04-09 07:41:14 +0800

    Update gatsby-config.ts
    Added mino fix to the config.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,105 +1,97 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-    trailingSlash: 'always',
-    siteMetadata: {
-        title: 'Magic Smiles Dental | Implant Dentistry | Linden, NJ',
-        author: 'moderndocmedia.com',
-        siteUrl: `https://www.lindennjdentist.com`,
-        description:
-            'Welcome to Magic Smiles Dental, home of the premier dentists in Linden, NJ. Offering comprehensive dental care for the entire family. Contact us today!',
-        keywords: 'Magic Smiles Dental, Dentist Linden, Linden NJ Dentist, lindennjdentist',
+  trailingSlash: 'always',
+  siteMetadata: {
+    title: 'Magic Smiles Dental | Implant Dentistry | Linden, NJ',
+    author: 'moderndocmedia.com',
+    siteUrl: `https://www.lindennjdentist.com`,
+    description:
+      'Welcome to Magic Smiles Dental, home of the premier dentists in Linden, NJ. Offering comprehensive dental care for the entire family. Contact us today!',
+    keywords: 'Magic Smiles Dental, Dentist Linden, Linden NJ Dentist, lindennjdentist',
+  },
+  graphqlTypegen: true,
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `./content/`,
+      },
     },
-    graphqlTypegen: true,
-    plugins: [
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sitemap`,
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-image`,
-        `gatsby-plugin-robots-txt`,
-        `gatsby-plugin-emotion`,
-        {
-            resolve: 'gatsby-plugin-svgr',
-            options: {
-                svgoConfig: {
-                    plugins: [
-                        {
-                            name: 'preset-default',
-                            params: {
-                                overrides: {
-                                    // disable plugins
-                                    removeViewBox: false,
-                                },
-                            },
-                        },
-                    ],
-                },
-            },
-        },
-        {
-            resolve: `gatsby-plugin-sharp`,
-            options: {
-                defaults: {
-                    placeholder: `blurred`,
-                    quality: 90,
-                    breakpoints: [380, 430, 750, 1080, 1366, 1920, 2000],
-                },
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Magic Smile Dental`,
-                short_name: `Magic Smile`,
-                start_url: `/`,
-                background_color: `#f5f5f5`,
-                theme_color: `#f5f5f5`,
-                display: `minimal-ui`,
-                icon: `assets/icons/favicon.png`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-canonical-urls`,
-            options: {
-                siteUrl: `https://www.lindennjdentist.com`,
-            },
-        },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `./assets/images/`,
+      },
+    },
 
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `./assets/images/`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-emotion`,
+
+    {
+      resolve: 'gatsby-plugin-svgr',
+      options: {
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
             },
+          ],
         },
-        {
-            resolve: 'gatsby-plugin-google-tagmanager',
-            options: {
-                id: 'GTM-MPQ958LV',
-                includeInDevelopment: true,
-            },
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          placeholder: `blurred`,
+          quality: 90,
+          breakpoints: [380, 430, 750, 1080, 1366, 1920, 2000],
         },
-        // {
-        //     resolve: `gatsby-plugin-google-gtag`,
-        //     options: {
-        //         trackingIds: ['GTM-MPQ958LV'],
-        //     },
-        // },
-        // {
-        //     resolve: `gatsby-plugin-mdx`,
-        //     options: {
-        //         extensions: [`.mdx`],
-        //         gatsbyRemarkPlugins: [`gatsby-remark-copy-linked-files`],
-        //     },
-        // },
-        // {
-        //     resolve: `gatsby-source-filesystem`,
-        //     options: {
-        //         name: `posts`,
-        //         path: `./src/content/blog/`,
-        //     },
-        // },
-    ],
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Magic Smile Dental`,
+        short_name: `Magic Smile`,
+        start_url: `/`,
+        background_color: `#f5f5f5`,
+        theme_color: `#f5f5f5`,
+        display: `minimal-ui`,
+        icon: `assets/icons/favicon.png`,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.lindennjdentist.com`,
+      },
+    },
+
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-MPQ958LV',
+        includeInDevelopment: true,
+      },
+    },
+  ],
 };
 
 export default config;
